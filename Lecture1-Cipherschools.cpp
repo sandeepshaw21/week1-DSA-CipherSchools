d754cdbaa7be4a120a0686013d218634ec304394
Sandeep Kumar Shaw <59821027+sandeepshaw21@users.noreply.github.com>
2023-06-25 15:10:13 +0530

    Add files via upload

---
+++ Lecture1-Cipherschools.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int sum(int n1,int n2){
+    return n1+n2;
+}
+
+int main(){
+    
+    int n1,n2;
+    cout<<"Enter First number ";
+    cin>>n1;
+    cout<<"Enter Second number ";
+    cin>>n2;
+
+    cout<<"The sum of First & Second number is : "<<sum(n1,n2);
+
+    return 0;
+}
